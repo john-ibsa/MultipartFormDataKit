f27bdfd41a5353414b162d83bb86c1580c0e1460
John Blomkamp <john@bitvice.io>
2020-11-10 15:13:59 +0200

    query

--- Sources/MultipartFormDataKit/Name.swift
+++ Sources/MultipartFormDataKit/Name.swift
@@ -12,7 +12,7 @@ public struct Name {
 
 
     public static func create(by filename: String) -> ValidationResult<Name, FailureReason> {
-        guard let percentEncodedString = filename.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+        guard let percentEncodedString = filename.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             return .invalid(because: .cannotPercentEncode(debugInfo: filename))
         }
 
